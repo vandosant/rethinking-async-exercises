ad5e5de044f4ecb2bb442b2e637b865a47473a66
vandosant <sjskender@gmail.com>
2016-06-23 13:56:38 -0600

    Solve ninth exercise - sampling with csp's

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
 	var $btn = $("#btn"),
 		$list = $("#list");
+	var chan = ASQ.csp.chan();
+        var msgs = ASQ.csp.chan();
+        var queued = false;
 
-	$btn.click(/* TODO */);
+	$btn.click(function(e) {
+	  if (!queued) {
+	    var pr = ASQ.csp.putAsync(chan, e)
+	    queued = true;
+	    pr.then(function() {
+              queued = false;
+	    })
+	  }
+	});
 
-	// Hint: ASQ().runner( .. )
-
+        ASQ().runner(
+          ASQ.csp.go(function *process1() {
+	    while (true) {
+	      yield ASQ.csp.take(ASQ.csp.timeout(1000));
+              yield ASQ.csp.take(chan);
+	      ASQ.csp.put(msgs, "clicked!");
+	    }
+	  }),
+	  ASQ.csp.go(function *process2() {
+            while (true) {
+              var msg = yield ASQ.csp.take(msgs);
+	      $list.append('<div>'+msg+'</div>');
+	    }
+	  })
+	)
 	// TODO: setup sampling go-routine and
 	// channel, populate $list
 });
